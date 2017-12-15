2a8701416206a60a1b171460e464e03f41b799d8
yeyuqiudeng <yeyuqiudeng@gmail.com>
2017-12-15 14:55:31 +0800

    feat: 增加考勤接口

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const { mixin } = require('./lib/util')
 const Common = require('./lib/common')
 const Department = require('./lib/department')
 const User = require('./lib/user')
+const Attendance = require('./lib/attendance')
 
-class DingApi extends mixin(Common, Department, User) {
+class DingApi extends mixin(
+  Common, 
+  Department, 
+  User,
+  Attendance
+) {
   constructor (getToken) {
     super()
     this.getToken = getToken
