5a19533d48e7852712bfc45cd57d859c3ba3db9d
yeyuqiudeng <yeyuqiudeng@gmail.com>
2017-12-27 19:15:30 +0800

    feat: getProcessInstance

--- test/bpms.spec.js
+++ test/bpms.spec.js
@@ -18,12 +18,19 @@ describe('Department', function () {
     })
   })
   describe('getAllProcessList()', function() {
-    it.only('should ok', async function () {
+    it('should ok', async function () {
       const result = await dingApi.getAllProcessList({
         process_code: 'PROC-FF6YBV6WQ2-OALFMUJAVETPRK3K89JS1-IBXY3BVI-N7',
         start_time: '2017-11-01'
       })
       assert.ok(Array.isArray(result))
+    })
+  })
+  describe('getProcessInstance()', function() {
+    it.only('should ok', async function () {
+      const result = await dingApi.getProcessInstance('585b22cc-61c6-4040-8c8a-fdd7d2505681')
+      console.log(JSON.stringify(result))
+      assert.equal(result.success, true)
     })
   })
 })
